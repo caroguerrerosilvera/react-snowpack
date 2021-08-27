0f4077ebb60382d7451b5c377ee1c0a62a0be18c
Caro Guerrero <caroguerrerosilvera@gmail.com>
2021-08-27 10:51:50 -0500

    feat: add optional chaining instruction

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const HelloWorld = () => (<h1>Hello from react with snowpack!</h1>);
+const HelloWorld = ({ data }) => {
+    const id = data?.id;
+    return (<h1>Hello from react with snowpack!</h1>);
+};
 
 export default HelloWorld;
